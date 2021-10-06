f7ca8a3f607d3862d1a6fdc0f418c3147f34d603
Renilson <86357654+renilsonjss@users.noreply.github.com>
2021-10-06 00:05:14 -0300

    Update banco.sql

--- Banco/banco.sql
+++ Banco/banco.sql
@@ -1,10 +1,16 @@
-CREATE DATABASE 'pweb_ajax';
-USE 'pweb_ajax';
+CREATE DATABASE pweb_ajax;
+USE pweb_ajax;
 
-CREATE TABLE 'USUARIO' 
+CREATE TABLE USUARIO 
 (
-  'ID_USUARIO' int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  'NOME' varchar(200),
-  'EMAIL' varchar(200),
-  'SENHA' varchar(32)
+  ID_USUARIO int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  NOME varchar(200),
+  EMAIL varchar(200),
+  SENHA varchar(32)
 );
+
+INSERT INTO USUARIO (NOME, EMAIL, SENHA) VALUES 
+('João', 'joao123@gmail.com', '123'),
+('José', 'jose456@gmail.com', '456'),
+('Maria', 'maria321@gmail.com', '321'),
+('Ana', 'ana654@gmail.com', '654');
